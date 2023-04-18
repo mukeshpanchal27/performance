e1e3cca808505ccd689b85ff5515fca8615a3247
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-04-18 15:49:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -67,6 +67,8 @@ svn update --set-depth infinity assets
 svn update --set-depth infinity trunk
 
 echo "SVN INFO"
+svn info tags
+
 #svn info "https://plugins.svn.wordpress.org/${SLUG}/tags/$VERSION"
 #content=$(svn info "https://plugins.svn.wordpress.org/${SLUG}/tags/$VERSION")
 
