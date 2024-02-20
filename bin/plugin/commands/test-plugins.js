c8f1e1a43827220e9112484d42bad7f56fe26b05
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2024-02-20 09:59:16 +0530

    Update test-plugins.js

--- bin/plugin/commands/test-plugins.js
+++ bin/plugin/commands/test-plugins.js
@@ -480,31 +480,35 @@ function doRunStandalonePluginTests( settings ) {
 
 	// Append plugins into array.
 	const plugins = pluginsConfig.plugins;
-	if ( plugins && Object.keys( plugins ).length > 0 ) {
-		plugins.forEach( ( plugin ) => {
-			// Copy plugins to build for testing.
-			try {
-				fs.copySync(
-					`${ settings.pluginsDir }${ plugin }/`,
-					`${ settings.builtPluginsDir }${ plugin }/`,
-					{
-						overwrite: true,
-					}
-				);
-				log( formats.success( `Copied plugin "${ plugin }".\n` ) );
-				builtPlugins = builtPlugins.concat( plugin );
-			} catch ( e ) {
+	if ( ! plugins ) {
+		log(
+			formats.error(
+				'The given plugin configuration is invalid, the plugins are missing, or they are misspelled.'
+			)
+		);
+
+		// Return with exit code 1 to trigger a failure in the test pipeline.
+		process.exit( 1 );
+	}
+
+	// Create an array of plugins from entries in plugins JSON file.
+	builtPlugins = builtPlugins.concat( Object.keys( plugins )
+		.filter( ( item ) => {
+			if (
+				! fs.pathExistsSync(
+					`${ settings.builtPluginsDir }${ plugins[ item ].slug }`
+				)
+			) {
 				log(
 					formats.error(
-						`Error copying plugin "${ plugin }". ${ e }`
+						`Built plugin path "${ settings.builtPluginsDir }${ plugins[ item ].slug }" not found, skipping and removing from plugin list`
 					)
 				);
-
-				// Return with exit code 1 to trigger a failure in the test pipeline.
-				process.exit( 1 );
+				return false;
 			}
-		} );
-	}
+			return true;
+		} )
+		.map( ( item ) => plugins[ item ].slug ));
 
 	// For each built plugin, copy the test assets.
 	builtPlugins.forEach( ( plugin ) => {
