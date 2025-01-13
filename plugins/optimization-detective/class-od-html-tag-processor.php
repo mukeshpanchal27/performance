aa07c71760223c608e156ad80b14124d9cf17ba6
Weston Ruter <westonruter@google.com>
2025-01-13 11:30:36 -0800

    Elaborate on rationale for XPath omitting node index on children of BODY

--- plugins/optimization-detective/class-od-html-tag-processor.php
+++ plugins/optimization-detective/class-od-html-tag-processor.php
@@ -103,10 +103,20 @@ final class OD_HTML_Tag_Processor extends WP_HTML_Tag_Processor {
 	 * in Web Almanac 2022, with the only exception being the very malformed tag name `script="async"`. Note that XPaths
 	 * begin with `/HTML/BODY` followed by an index-free reference to an element which is a direct child of the BODY,
 	 * for example `/HTML/BODY/DIV`. Below this point, all tags must then have indices to disambiguate the XPaths among
-	 * siblings. For example: `/HTML/BODY/DIV/*[2][self::MAIN]/*[1][self::FIGURE]/*[2][self::IMG]`. There is no need
+	 * siblings. For example: `/HTML/BODY/DIV/*[2][self::MAIN]/*[1][self::FIGURE]/*[2][self::IMG]`. There is little need
 	 * for there to be an index added to the `DIV` directly under the `BODY` because WordPress themes almost always use
 	 * a wrapper element: Block Themes always wrap the page in a `DIV.wp-site-blocks` element, and classic themes either
 	 * wrap the content in a `DIV#page` or else use `HEADER`, `MAIN`, and `FOOTER`.
+	 *
+	 * The benefit of omitting the node index from direct children of the BODY allows for variations in content output
+	 * at `wp_body_open()` without impacting the computed XPaths for subsequent tags. Omitting the node index at this
+	 * level, however, does incur a slight risk of duplicate XPaths being computed. For example, if a theme has a
+	 * `<div id="header" role="banner">` and a `<div id="footer" role="contentinfo">` which are both direct descendants
+	 * of `BODY`, then it is possible for an XPath like `/HTML/BODY/DIV/*[1][self::IMG]` to be duplicated if both of
+	 * these `DIV` elements has an `IMG` as the first child. The conflict could arise for any tags of the same name in
+	 * the same relative position, which does not seem likely. Additionally, as noted above, themes almost always wrap
+	 * the page content in an overall `DIV` or else they use semantic HTML tags like `HEADER` and `FOOTER`, so the risk
+	 * is low.
 	 *
 	 * @since 0.4.0
 	 * @see self::get_xpath()
