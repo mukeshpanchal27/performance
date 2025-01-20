aec6f80fc3a89d3c207a7c22b6d509d66fd904fd
Aditya Dhade <aditya.dhade@rtcamp.com>
2025-01-20 17:51:55 +0530

    Add additional tests to improve code coverage

--- plugins/performance-lab/tests/includes/site-health/far-future-headers/test-far-future-headers.php
+++ plugins/performance-lab/tests/includes/site-health/far-future-headers/test-far-future-headers.php
@@ -29,6 +29,32 @@ class Test_Far_Future_Headers extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test that the far-future headers test is added to the site health tests.
+	 *
+	 * @covers ::perflab_ffh_add_test
+	 */
+	public function test_perflab_ffh_add_test(): void {
+		$tests = array(
+			'direct' => array(),
+		);
+
+		$tests = perflab_ffh_add_test( $tests );
+
+		$this->assertArrayHasKey( 'far_future_headers', $tests['direct'] );
+		$this->assertEquals( 'Effective Caching Headers', $tests['direct']['far_future_headers']['label'] );
+		$this->assertEquals( 'perflab_ffh_assets_test', $tests['direct']['far_future_headers']['test'] );
+	}
+
+	/**
+	 * Test that the far-future headers test is attached to the site status tests.
+	 *
+	 * @covers ::perflab_ffh_add_test
+	 */
+	public function test_perflab_ffh_add_test_is_attached_to_site_status_tests(): void {
+		$this->assertNotFalse( has_filter( 'site_status_tests', 'perflab_ffh_add_test' ) );
+	}
+
+	/**
 	 * Test that when all assets have valid far-future headers, the status is "good".
 	 *
 	 * @covers ::perflab_ffh_assets_test
@@ -61,22 +87,83 @@ class Test_Far_Future_Headers extends WP_UnitTestCase {
 	public function test_assets_conditionally_cached(): void {
 		// For conditional caching scenario, setting etag/last-modified headers.
 		$this->mocked_responses = array(
-			'js/wp-embed.min.js'     => $this->build_response( 200, array( 'cache-control' => 'max-age=' . ( YEAR_IN_SECONDS + 1000 ) ) ),
-			'css/buttons.min.css'    => $this->build_response( 200, array( 'etag' => '"123456789"' ) ),
-			'fonts/dashicons.woff2'  => $this->build_response( 200, array( 'last-modified' => gmdate( 'D, d M Y H:i:s', time() - 1000 ) . ' GMT' ) ),
-			'images/media/video.png' => $this->build_response(
+			includes_url( 'js/wp-embed.min.js' )     => $this->build_response( 200, array( 'cache-control' => 'max-age=' . ( YEAR_IN_SECONDS + 1000 ) ) ),
+			includes_url( 'css/buttons.min.css' )    => $this->build_response( 200, array( 'etag' => '"123456789"' ) ),
+			includes_url( 'fonts/dashicons.woff2' )  => $this->build_response( 200, array( 'last-modified' => gmdate( 'D, d M Y H:i:s', time() - 1000 ) . ' GMT' ) ),
+			includes_url( 'images/media/video.png' ) => $this->build_response(
 				200,
 				array(
 					'etag'          => '"123456789"',
 					'last-modified' => gmdate( 'D, d M Y H:i:s', time() - 1000 ) . ' GMT',
 				)
 			),
-			'conditional_304'        => $this->build_response( 304 ),
+			'conditional_304'                        => $this->build_response( 304 ),
 		);
 
 		$result = perflab_ffh_assets_test();
 		$this->assertEquals( 'recommended', $result['status'] );
 		$this->assertNotEmpty( $result['actions'] );
+	}
+
+	/**
+	 * Test that ETag/Last-Modified is used for conditional requests.
+	 *
+	 * @dataProvider data_provider_conditional_headers
+	 * @covers ::perflab_ffh_try_conditional_request
+	 *
+	 * @param string                        $url      The URL to test.
+	 * @param array<string, string>         $headers  The headers to send.
+	 * @param array<string, mixed>|WP_Error $response The response to return.
+	 * @param bool                          $expected The expected result.
+	 */
+	public function test_try_conditional_request_function( string $url, array $headers, $response, bool $expected ): void {
+		$this->mocked_responses = array(
+			$url => $response,
+		);
+
+		$result = perflab_ffh_try_conditional_request( $url, $headers );
+
+		$this->assertEquals( $expected, $result );
+	}
+
+	/**
+	 * Data provider for test_try_conditional_request_function.
+	 *
+	 * @return array<array<mixed>> Data provider.
+	 */
+	public function data_provider_conditional_headers(): array {
+		return array(
+			array(
+				includes_url( 'js/wp-embed.min.js' ),
+				array( 'If-None-Match' => '"123456789"' ),
+				$this->build_response( 304 ),
+				true,
+			),
+			array(
+				includes_url( 'css/buttons.min.css' ),
+				array( 'If-Modified-Since' => gmdate( 'D, d M Y H:i:s', time() - 1000 ) . ' GMT' ),
+				$this->build_response( 304 ),
+				true,
+			),
+			array(
+				includes_url( 'fonts/dashicons.woff2' ),
+				array( 'If-None-Match' => '"123456789"' ),
+				$this->build_response( 200 ),
+				false,
+			),
+			array(
+				includes_url( 'images/media/video.png' ),
+				array( 'If-Modified-Since' => gmdate( 'D, d M Y H:i:s', time() - 1000 ) . ' GMT' ),
+				$this->build_response( 200 ),
+				false,
+			),
+			array(
+				includes_url( 'images/media/video.png' ),
+				array(),
+				new WP_Error( 'http_request_failed', 'HTTP request failed' ),
+				false,
+			),
+		);
 	}
 
 	/**
@@ -90,25 +177,40 @@ class Test_Far_Future_Headers extends WP_UnitTestCase {
 		$this->mocked_responses = array(
 			includes_url( 'js/wp-embed.min.js' )     => $this->build_response( 200, array( 'cache-control' => 'max-age=' . ( YEAR_IN_SECONDS - 1000 ) ) ),
 			includes_url( 'css/buttons.min.css' )    => $this->build_response( 200, array( 'expires' => gmdate( 'D, d M Y H:i:s', time() + YEAR_IN_SECONDS - 1000 ) . ' GMT' ) ),
+			includes_url( 'images/blank.gif' )       => $this->build_response(
+				200,
+				array(
+					'cache-control' => 'max-age=' . ( YEAR_IN_SECONDS - 1000 ),
+					'expires'       => gmdate( 'D, d M Y H:i:s', time() - 1000 ) . ' GMT',
+				)
+			),
+			'conditional_304'                        => $this->build_response( 304 ),
 			includes_url( 'fonts/dashicons.woff2' )  => $this->build_response( 200, array( 'etag' => '"123456789"' ) ),
 			includes_url( 'images/media/video.png' ) => $this->build_response( 200, array() ),
-			'conditional_304'                        => $this->build_response( 304 ),
+			includes_url( 'images/media/video.svg' ) => new WP_Error( 'http_request_failed', 'HTTP request failed' ),
+			includes_url( 'images/media/code.png' )  => array(),
 		);
 
 		$result = perflab_ffh_check_assets(
 			array(
 				includes_url( 'js/wp-embed.min.js' ),
 				includes_url( 'css/buttons.min.css' ),
+				includes_url( 'images/blank.gif' ),
 				includes_url( 'fonts/dashicons.woff2' ),
 				includes_url( 'images/media/video.png' ),
+				includes_url( 'images/media/video.svg' ),
+				includes_url( 'images/media/code.png' ),
 			)
 		);
 
 		$this->assertEquals( 'recommended', $result['final_status'] );
 		$this->assertStringContainsString( 'max-age below threshold (actual:', $result['details'][0]['reason'] );
 		$this->assertStringContainsString( 'expires below threshold (actual:', $result['details'][1]['reason'] );
-		$this->assertEquals( 'No far-future headers but conditionally cached', $result['details'][2]['reason'] );
-		$this->assertEquals( 'No far-future headers and no conditional caching', $result['details'][3]['reason'] );
+		$this->assertStringContainsString( 'max-age below threshold (actual:', $result['details'][2]['reason'] );
+		$this->assertEquals( 'No far-future headers but conditionally cached', $result['details'][3]['reason'] );
+		$this->assertEquals( 'No far-future headers and no conditional caching', $result['details'][4]['reason'] );
+		$this->assertEquals( 'Could not retrieve headers', $result['details'][5]['reason'] );
+		$this->assertEquals( 'No valid headers retrieved', $result['details'][6]['reason'] );
 	}
 
 	/**
@@ -177,9 +279,9 @@ class Test_Far_Future_Headers extends WP_UnitTestCase {
 	 * @param bool                 $response A preemptive return value of an HTTP request. Default false.
 	 * @param array<string, mixed> $args     Request arguments.
 	 * @param string               $url      The request URL.
-	 * @return array<string, mixed> Mocked response.
+	 * @return array<string, mixed>|WP_Error Mocked response.
 	 */
-	public function mock_http_requests( bool $response, array $args, string $url ): array {
+	public function mock_http_requests( bool $response, array $args, string $url ) {
 		// If conditional headers used in second request, simulate a 304 response.
 		if ( isset( $this->mocked_responses['conditional_304'] ) && ( isset( $args['headers']['If-None-Match'] ) || isset( $args['headers']['If-Modified-Since'] ) ) ) {
 			return $this->mocked_responses['conditional_304'];
