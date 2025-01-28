b002bff4ee0d0d29540f68b266b836e5e6c2eb03
Shyamsundar Gadde <shyamsundar.gadde@rtcamp.com>
2025-01-29 00:23:10 +0530

    Fix PHP fatal error caused by duplicate trait declaration
    Signed-off-by: Shyamsundar Gadde <shyamsundar.gadde@rtcamp.com>

--- plugins/embed-optimizer/tests/bootstrap.php
+++ plugins/embed-optimizer/tests/bootstrap.php
@@ -9,4 +9,4 @@
 require_once __DIR__ . '/../../optimization-detective/load.php';
 
 // Load the test helpers.
-require_once __DIR__ . '/../../optimization-detective/tests/class-optimization-detective-test-helpers.php';
+require_once TESTS_REPO_ROOT_DIR . '/plugins/optimization-detective/tests/class-optimization-detective-test-helpers.php';
--- plugins/image-prioritizer/tests/bootstrap.php
+++ plugins/image-prioritizer/tests/bootstrap.php
@@ -6,4 +6,4 @@
  */
 
 // Load the test helpers.
-require_once __DIR__ . '/../../optimization-detective/tests/class-optimization-detective-test-helpers.php';
+require_once TESTS_REPO_ROOT_DIR . '/plugins/optimization-detective/tests/class-optimization-detective-test-helpers.php';
