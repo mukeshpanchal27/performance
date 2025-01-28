6d26270a19e4f6191bb8827cb87f2ee580b0f198
Shyamsundar Gadde <shyamsundar.gadde@rtcamp.com>
2025-01-29 00:57:01 +0530

    Fix incorrect plugin path
    Signed-off-by: Shyamsundar Gadde <shyamsundar.gadde@rtcamp.com>

--- plugins/embed-optimizer/tests/bootstrap.php
+++ plugins/embed-optimizer/tests/bootstrap.php
@@ -6,7 +6,7 @@
  */
 
 // Require the suggested plugin.
-require_once TESTS_REPO_ROOT_DIR . '/optimization-detective/load.php';
+require_once TESTS_REPO_ROOT_DIR . '/plugins/optimization-detective/load.php';
 
 // Load the test helpers.
 require_once TESTS_REPO_ROOT_DIR . '/plugins/optimization-detective/tests/class-optimization-detective-test-helpers.php';
